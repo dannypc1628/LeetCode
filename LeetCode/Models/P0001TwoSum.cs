a9f6fa2a3a3520c27b5fc1422a2504e66f5554ff
dannyLiu <h152074716@gmail.com>
2020-08-30 14:41:25 +0800

    第一題 Two Sum

---
+++ LeetCode/Models/P0001TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0001TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            return Go(nums, target);
+        }
+
+        private int[] Go(int[] nums, int target)
+        {
+            var a = 0;
+            var b = 0;
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                for (var j = i+1; j < nums.Length;j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        a = i;
+                        b = j;
+                        break;
+                    }
+                }
+            }
+            return new []{a,b};
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0001TwoSumTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0001TwoSumTests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            var nums = new[] { 2, 7, 11, 15 };
+            var target = 9;
+            var expect = new [] { 0, 1 };
+            var test = new LeetCode.Models.P0001TwoSum();
+            
+            var actual = test.TwoSum(nums, target);
+            CollectionAssert.AreEqual(expect,actual);
+        }
+    }
+}
