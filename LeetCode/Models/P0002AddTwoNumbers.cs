18e7ad2a8196d7bec0aa12d861da949186c253b1
dannyLiu <h152074716@gmail.com>
2020-09-02 23:49:45 +0800

    第二題 Add Two Numbers

---
+++ LeetCode/Models/P0002AddTwoNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0002AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var sum = new ListNode
+            {
+                val = l1.val + l2.val
+            };
+            var q = sum.val / 10;
+            sum.val %= 10;
+            if (l1.next != null && l2.next != null)
+            {
+                l1.next.val += q;
+                sum.next = AddTwoNumbers(l1.next, l2.next);
+            }
+            else if (l1.next == null && l2.next == null)
+            {
+                if (q > 0)
+                    sum.next = new ListNode(1);
+            }
+            else
+            {
+                if (l1.next != null && l2.next == null)
+                {
+                    l2.next = new ListNode(q);
+                    sum.next = AddTwoNumbers(l1.next, l2.next);
+                }
+                else
+                {
+                    l1.next = new ListNode(q);
+                    sum.next = AddTwoNumbers(l1.next, l2.next);
+                }
+
+            }
+            return sum;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0002AddTwoNumbersTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0002AddTwoNumbersTests
+    {
+        [TestMethod()]
+        public void AddTwoNumbersTest()
+        {
+            var l1 = new ListNode(2)
+            {
+                next = new ListNode(4)
+            };
+            l1.next.next = new ListNode(3);
+
+            var l2 = new ListNode(5)
+            {
+                next = new ListNode(6)
+            };
+            l2.next.next = new ListNode(4);
+
+            var p2 = new P0002AddTwoNumbers();
+
+            var res = p2.AddTwoNumbers(l1, l2);
+            var expect = new List<int> { 7, 0, 8 };
+            var actual = new List<int>();
+
+            var now = res;
+            while (true)
+            {
+                actual.Add(now.val);
+                if (now.next != null)
+                {
+                    now = now.next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            CollectionAssert.AreEqual(expect,actual);
+
+        }
+    }
+}
