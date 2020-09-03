62c099fdbeadf206285a902c87e8a2862b1d73db
dannyLiu <h152074716@gmail.com>
2020-09-03 23:36:59 +0800

    第三題Longest Substring Without Repeating Characters

---
+++ LeetCode/Models/P0003LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0003LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var longest = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                var dictionary = new Dictionary<char, int>();
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (dictionary.ContainsKey(s[j]))
+                    {
+                        var sub = s.Substring(i, j - i);
+                        if (sub.Length > longest.Length)
+                        {
+                            longest = sub;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        dictionary.Add(s[j], 1);
+                        var sub = s.Substring(i, j - i+1);
+                        if (sub.Length > longest.Length)
+                        {
+                            longest = sub;
+                        }
+                    }
+                }
+            }
+            return longest.Length;
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0003LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0003LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest_Give_abcabcbb_Get_3()
+        {
+            var input = "abcabcbb";
+            var expect = 3;
+            var test = new P0003LongestSubstringWithoutRepeatingCharacters();
+            var actual = test.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(expect,actual);
+        }
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest_Give_bbbbb_Get_1()
+        {
+            var input = "bbbbb";
+            var expect = 1;
+            var test = new P0003LongestSubstringWithoutRepeatingCharacters();
+            var actual = test.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest_Give_Empty_Get_0()
+        {
+            var input = "";
+            var expect = 0;
+            var test = new P0003LongestSubstringWithoutRepeatingCharacters();
+            var actual = test.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest_Give_pwwkew_Get_3()
+        {
+            var input = "pwwkew";
+            var expect = 3;
+            var test = new P0003LongestSubstringWithoutRepeatingCharacters();
+            var actual = test.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest_Give_WhiteSpace_Get_1()
+        {
+            var input = " ";
+            var expect = 1;
+            var test = new P0003LongestSubstringWithoutRepeatingCharacters();
+            var actual = test.LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
