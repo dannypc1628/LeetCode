340ca0aca0a52fe6f1363e4eea4e6fa3da88b35f
dannyLiu <h152074716@gmail.com>
2020-09-04 23:51:17 +0800

    第45題 Jump Game II

---
+++ LeetCode/Models/P0045JumpGameII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0045JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums.Length == 1)
+                return 0;
+            return Go(nums, 0, 0);
+        }
+
+        private int Go(int[] nums, int point, int count)
+        {
+            var maxLength = nums[point];
+            if (point + maxLength >= nums.Length - 1)
+                return count + 1;
+
+            var nextPoint = 0;
+            for (int i = 1; i <= maxLength; i++)
+            {
+                var nowPoint = point + i;
+                if (nums[nowPoint] + nowPoint >= nums[nextPoint] + nextPoint)
+                {
+                    nextPoint = nowPoint;
+                }
+            }
+
+            return Go(nums, nextPoint, count + 1);
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0045JumpGameIITests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0045JumpGameIITests
+    {
+        [TestMethod()]
+        public void Give_23114_Get_2()
+        {
+            var nums = new[] { 2, 3, 1, 1, 4 };
+            var expect = 2;
+            var test = new P0045JumpGameII();
+            var actual = test.Jump(nums);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void Give_0_Get_0()
+        {
+            var nums = new[] { 0 };
+            var expect = 0;
+            var test = new P0045JumpGameII();
+            var actual = test.Jump(nums);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void Give_1111_Get_3()
+        {
+            var nums = new[] { 1, 1, 1, 1 };
+            var expect = 3;
+            var test = new P0045JumpGameII();
+            var actual = test.Jump(nums);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void Give_1098765432110_Get_3()
+        {
+            var nums = new[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0 };
+            var expect = 2;
+            var test = new P0045JumpGameII();
+            var actual = test.Jump(nums);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
