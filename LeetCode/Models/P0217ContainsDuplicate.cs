bb13fa4207ec4db1f6bb7eae38d91609bf6c6ea5
dannyLiu <h152074716@gmail.com>
2020-09-04 12:56:59 +0800

    第217題 Contains Duplicate

---
+++ LeetCode/Models/P0217ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0217ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (set.Contains(num))
+                    return true;
+                else
+                {
+                    set.Add(num);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0217ContainsDuplicateTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0217ContainsDuplicateTests
+    {
+        [TestMethod()]
+        public void Give_1231_Get_True()
+        {
+            var nums = new[] { 1, 2, 3, 1 };
+            var expect = true;
+            var test = new P0217ContainsDuplicate();
+            var actual = test.ContainsDuplicate(nums);
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void Give_1234_Get_False()
+        {
+            var nums = new[] { 1, 2, 3, 4 };
+            var expect = false;
+            var test = new P0217ContainsDuplicate();
+            var actual = test.ContainsDuplicate(nums);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
