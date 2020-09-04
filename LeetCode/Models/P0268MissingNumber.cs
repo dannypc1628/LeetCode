5be3624f9542abd6d7b017e57663bd696776a123
dannyLiu <h152074716@gmail.com>
2020-09-04 15:33:05 +0800

    第268題 MissingNumber

---
+++ LeetCode/Models/P0268MissingNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0268MissingNumber
+    {
+        public int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            var i = 0;
+            foreach (var num in nums)
+            {
+                if (i != num)
+                    return i;
+                i++;
+            }
+            return i;
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0268MissingNumberTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0268MissingNumberTests
+    {
+        [TestMethod()]
+        public void Give_301_Get_2()
+        {
+            var nums = new[] { 3, 0, 1 };
+            var expect = 2;
+            var test = new P0268MissingNumber();
+            var actual = test.MissingNumber(nums);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void Give_964235701_Get_8()
+        {
+            var nums = new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            var expect = 8;
+            var test = new P0268MissingNumber();
+            var actual = test.MissingNumber(nums);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void Give_0_Get_1()
+        {
+            var nums = new[] { 0 };
+            var expect = 1;
+            var test = new P0268MissingNumber();
+            var actual = test.MissingNumber(nums);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
