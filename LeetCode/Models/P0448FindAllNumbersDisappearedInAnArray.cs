6d39c62d345629458022d5b909c18642bfaa0884
dannyLiu <h152074716@gmail.com>
2020-09-05 16:46:57 +0800

    第448題 Find All Numbers Disappeared in an Array

---
+++ LeetCode/Models/P0448FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0448FindAllNumbersDisappearedInAnArray
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            if (nums.Length > 0)
+            {
+                var count = nums.Length;
+
+                var set = new HashSet<int>();
+                for (var i = 1; i <= count; i++)
+                {
+                    set.Add(i);
+                }
+
+                foreach (var num in nums)
+                {
+                    set.Remove(num);
+                }
+
+                return set.ToList();
+            }
+            var list = new List<int>();
+            return list;
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0448FindAllNumbersDisappearedInAnArrayTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0448FindAllNumbersDisappearedInAnArrayTests
+    {
+        [TestMethod()]
+        public void Give_43278231_Get_56()
+        {
+            var nums = new[] {4, 3, 2, 7, 8, 2, 3, 1};
+            var expect = new [] {5, 6};
+            var test = new P0448FindAllNumbersDisappearedInAnArray();
+            var actual = test.FindDisappearedNumbers(nums);
+            CollectionAssert.AreEqual(expect,actual.ToArray());
+        }
+
+        [TestMethod()]
+        public void Give_11_Get_2()
+        {
+            var nums = new[] { 1, 1 };
+            var expect = new[] { 2 };
+            var test = new P0448FindAllNumbersDisappearedInAnArray();
+            var actual = test.FindDisappearedNumbers(nums);
+            CollectionAssert.AreEqual(expect, actual.ToArray());
+        }
+    }
+}
