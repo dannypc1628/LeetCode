257034de43a38a856045398a4e1aedcab076a615
dannyLiu <h152074716@gmail.com>
2020-09-05 18:06:20 +0800

    第 136題 Single Number

---
+++ LeetCode/Models/P0136SingleNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0136SingleNumber
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var set = new HashSet<int>();
+            
+            foreach (var num in nums)
+            {
+                if (set.Contains(num))
+                {
+                    set.Remove(num);
+                }
+                else
+                {
+                    set.Add(num);
+                }
+            }
+
+            return set.FirstOrDefault();
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0136SingleNumberTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0136SingleNumberTests
+    {
+        [TestMethod()]
+        public void Give_221_Get_1()
+        {
+            var nums = new[] { 2,2,1 };
+            var expect = 1;
+            var test = new P0136SingleNumber();
+            var actual = test.SingleNumber(nums);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void Give_41212_Get_1()
+        {
+            var nums = new[] { 4, 1, 2, 1, 2 };
+            var expect = 4;
+            var test = new P0136SingleNumber();
+            var actual = test.SingleNumber(nums);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
