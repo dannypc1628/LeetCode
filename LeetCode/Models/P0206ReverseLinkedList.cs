e3509d3770012c18632f4c19069dd21bba959a8e
dannyLiu <h152074716@gmail.com>
2020-09-13 17:57:53 +0800

    第206題 Reverse Linked List

---
+++ LeetCode/Models/P0206ReverseLinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0206ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            var stack = new Stack<ListNode>();
+
+            var node = head;
+            while (node!=null)
+            {
+                stack.Push(node);
+                node = node.next;
+            }
+
+            var ans = new ListNode(stack.Pop().val);
+            var point = ans;
+            while (stack.Count>0)
+            {
+                point.next = new ListNode(stack.Pop().val);
+                point = point.next;
+            }
+
+            return ans;
+        }
+    }
+    
+}
---
+++ LeetCodeTests/Models/P0206ReverseLinkedListTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0206ReverseLinkedListTests
+    {
+        [TestMethod()]
+        public void Give_12345_Get_54321()
+        {
+            var support = new P0002AddTwoNumbersTests();
+            var test = new P0206ReverseLinkedList();
+
+            var input = support.GetListNode(new[] { 1, 2, 3, 4, 5 });
+            var expect = new[] { 5, 4, 3, 2, 1 };
+            var output = test.ReverseList(input);
+            var actual = ConvertListNodeToArray(output);
+            CollectionAssert.AreEqual(expect,actual);
+        }
+        [TestMethod()]
+        public void Give_null_Get_null()
+        {
+            var support = new P0002AddTwoNumbersTests();
+            var test = new P0206ReverseLinkedList();
+
+            var actual = test.ReverseList(null);
+            Assert.AreEqual(null, actual);
+        }
+
+        private int[] ConvertListNodeToArray(ListNode node)
+        {
+            var arr = new List<int>();
+            var point = node;
+            while (point != null)
+            {
+                arr.Add(point.val);
+                point = point.next;
+            }
+
+            return arr.ToArray();
+        }
+    }
+}
