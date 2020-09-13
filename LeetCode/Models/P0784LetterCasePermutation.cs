b03bdc68c4071dce44bc94efbdecdfac84a58480
dannyLiu <h152074716@gmail.com>
2020-09-13 16:58:19 +0800

    第784題 Letter Case Permutation

---
+++ LeetCode/Models/P0784LetterCasePermutation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Models
+{
+    public class P0784LetterCasePermutation
+    {
+        public IList<string> LetterCasePermutation(string s)
+        {
+            return GetCase(s);
+        }
+
+        private IList<string> GetCase(string s)
+        {
+            var point = 0;
+            foreach (var c in s)
+            {
+                if (char.IsLetter(c))
+                {
+                    break;
+                }
+                point++;
+            }
+            if (point == s.Length)
+                return new List<string>() { s };
+
+            var sEndList = GetCase(s.Substring(point+1));
+            var sub = s.Substring(0, point);
+            var list = new List<string>();
+
+            foreach (var strItem in sEndList)
+            {
+                list.Add(sub + char.ToUpper(s[point]) + strItem);
+                list.Add(sub + char.ToLower(s[point]) + strItem);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0784LetterCasePermutationTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0784LetterCasePermutationTests
+    {
+        [TestMethod()]
+        public void Give_1a2b()
+        {
+            var test = new P0784LetterCasePermutation();
+            var s = "1a2b";
+            var actual = test.LetterCasePermutation(s);
+
+            var expect = new List<string>()
+            {
+                "1A2B",
+                "1a2B",
+                "1A2b",
+                "1a2b",
+            };
+            CollectionAssert.AreEqual(expect.ToArray(),actual.ToArray());
+        }
+    }
+}
