067512d93b94228928e22df215a98e8401e78cba
dannyLiu <h152074716@gmail.com>
2022-02-20 16:30:56 +0800

    第14題 Longest Common Prefix

---
+++ LeetCode/Models/P0014LongestCommonPrefix.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Models
+{
+    public class P0014LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var temp = string.Empty;
+            for (var i = 0; i <= 200; i++)
+            {
+                var value = commonChar(strs, i);
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    return temp;
+                }
+                temp += value;
+            }
+
+            return temp;
+        }
+
+        private string commonChar(string[] strs, int i)
+        {
+            var temp = string.Empty;
+            var count = 0;
+            foreach (var str in strs)
+            {
+                if (i + 1 > str.Length)
+                {
+                    return string.Empty;
+                }
+
+                if (count == 0)
+                {
+                    temp = str.Substring(i, 1);
+                    count++;
+                }
+
+                if (str.Substring(i, 1) != temp)
+                {
+                    return string.Empty;
+                }
+
+            }
+
+            return temp;
+
+        }
+    }
+}
---
+++ LeetCodeTests/Models/P0014LongestCommonPrefixTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Models.Tests
+{
+    [TestClass()]
+    public class P0014LongestCommonPrefixTests
+    {
+        [TestMethod()]
+        public void LongestCommonPrefixTest_Expect_String()
+        {
+            // Arrange
+            var data1 = new[] { "flower", "flow", "flight" };
+            var test = new P0014LongestCommonPrefix();
+
+            // Act
+            var result1 = test.LongestCommonPrefix(data1);
+
+            // Assert
+            Assert.AreEqual("fl", result1);
+        }
+
+        [TestMethod()]
+        public void LongestCommonPrefixTest_Expect_Empty()
+        {
+            // Arrange
+            var data2 = new[] { "dog", "racecar", "car" };
+            var test = new P0014LongestCommonPrefix();
+
+            // Act
+            var result2 = test.LongestCommonPrefix(data2);
+
+            // Assert
+            Assert.AreEqual("", result2);
+        }
+
+    }
+}
